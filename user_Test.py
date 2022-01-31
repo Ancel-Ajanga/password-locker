bb7c2a8e26a41bbf75c1bbcb97e7d1e1e9b368db
Ancel <ancel.ajanga@student.moringaschool.com>
2022-01-31 09:42:19 +0300

    add find user

--- user_Test.py
+++ user_Test.py
@@ -57,3 +57,21 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.new_user.delete_user()
         self.assertEqual(len(User.user_list), 1)
+
+
+###########5th test###########
+
+    def test_find_user(self):
+        '''
+        find a user using username
+        '''
+        self.new_user.save_user()
+        test_user = User("test", "password")
+        test_user.save_user()
+        found_user = User.find_user("vincentouma")
+        self.assertEqual(found_user.username, self.new_user.username)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
