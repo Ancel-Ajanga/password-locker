87659c669c8c6178045471bc6949d026cd16e938
Ancel <ancel.ajanga@student.moringaschool.com>
2022-01-31 09:51:44 +0300

    save user

--- run.py
+++ run.py
@@ -11,3 +11,18 @@ def create_useraccount(username, password):
     '''
     new_user = User(username, password)
     return new_user
+
+     ######save user#####
+def save_user(user):
+    '''
+    method save user  account
+    '''
+    user.save_user()
+
+def save_credentials(credentials):
+
+    '''
+    method save credentials  account
+    '''
+
+    credentials.save_credentials
