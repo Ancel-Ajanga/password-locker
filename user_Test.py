3742497a5dc4abac0a22022d02ddec4b761452ee
Ancel <ancel.ajanga@student.moringaschool.com>
2022-01-31 09:16:24 +0300

    add new user

--- user_Test.py
+++ user_Test.py
@@ -1,3 +1,11 @@
 import unittest # Importing the unittest module
 from user import User # Importing the contact class
 class TestUser(unittest.TestCase):
+
+
+
+       def setUp(self):
+        '''
+        method to run before each test
+        '''
+        self.new_user=User("dukelemayian", "lemayian1005") #new User created
